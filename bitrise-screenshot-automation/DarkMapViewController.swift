a79fb23fad7754ad69a98370fc7d8c8d5eec8e85
Rishab Sukumar <rishab.sukumar@chargepoint.com>
2022-10-04 16:14:49 -0700

    [IMA-12875] Add Zoom functionality for a few cities to main map view

--- bitrise-screenshot-automation/DarkMapViewController.swift
+++ bitrise-screenshot-automation/DarkMapViewController.swift
@@ -9,9 +9,32 @@
 import MapKit
 import UIKit
 
-class DarkMapViewController: UIViewController, CLLocationManagerDelegate {
+enum Location {
+    case Default
+    case SanFrancisco
+    case NewYork
+    case Chicago
+    
+    var description: String {
+        switch self {
+        case .Default:
+            return "Default"
+        case .SanFrancisco:
+            return "San Francisco"
+        case .NewYork:
+            return "New York"
+        case .Chicago:
+            return "Chicago"
+        }
+    }
+}
+
+class DarkMapViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    var locationList: [Location] = [.Default, .SanFrancisco, .NewYork, .Chicago]
+    var coordinateDict: Dictionary<Location, (Double, Double)> = [.Default: (54.5260, -105.2551), .SanFrancisco: (37.7749, -122.4194), .NewYork: (40.7128, -74.0060), .Chicago: (41.8781, -87.6298)]
+    
     @IBOutlet weak var mapView: MKMapView?
-    @IBOutlet weak var cityLabel: UILabel?
+    @IBOutlet var locationPickerView: UIPickerView!
     @IBOutlet weak var zoomButton: UIButton?
 
     var locationManager: CLLocationManager?
@@ -19,6 +42,8 @@ class DarkMapViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.locationPickerView.delegate = self
+        self.locationPickerView.dataSource = self
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -40,6 +65,39 @@ class DarkMapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBAction func zoomToCityTapped() {
         // Do nothing for now
+        let city = self.locationList[self.locationPickerView.selectedRow(inComponent: 0)]
+        switch city {
+        case .Default:
+            let (centerLatitude, centerLongitude) = self.coordinateDict[.Default] ?? (0, 0)
+            zoomToLocation(latitude: centerLatitude, longitude: centerLongitude, latitudinalMeters: 6000000, longitudinalMeters: 6000000)
+        case .SanFrancisco:
+            let (centerLatitude, centerLongitude) = self.coordinateDict[.SanFrancisco] ?? (0, 0)
+            zoomToLocation(latitude: centerLatitude, longitude: centerLongitude, latitudinalMeters: 50000, longitudinalMeters: 50000)
+        case .NewYork:
+            let (centerLatitude, centerLongitude) = self.coordinateDict[.NewYork] ?? (0, 0)
+            zoomToLocation(latitude: centerLatitude, longitude: centerLongitude, latitudinalMeters: 50000, longitudinalMeters: 50000)
+        case .Chicago:
+            let (centerLatitude, centerLongitude) = self.coordinateDict[.Chicago] ?? (0, 0)
+            zoomToLocation(latitude: centerLatitude, longitude: centerLongitude, latitudinalMeters: 50000, longitudinalMeters: 50000)
+        }
+    }
+    
+    func zoomToLocation(latitude: Double, longitude: Double, latitudinalMeters: Double, longitudinalMeters: Double) {
+        let center: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: latitudinalMeters, longitudinalMeters: longitudinalMeters)
+        self.mapView?.setRegion(region, animated: true)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.locationList[row].description
     }
 
 }
